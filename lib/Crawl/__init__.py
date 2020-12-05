08148970925f56ac9251fdd62db359ff86df6b8d
= <=>
2020-12-05 15:20:49 +0700

    update Web UI

---
+++ lib/Crawl/__init__.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('/media/lahai/DATA/Study/DAI HOC/NamBa/TruyVan/Tuan4/Crawl')
+
+
+from .Crawl import Crawl
+from .CrawlVnE import CrawlVnE
+from .CrawlWiki import CrawlWiki
+from .CData import CData
